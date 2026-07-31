e80e0e226df107b13aeb134530c3d573dfe1a293
lakhant99 <lakhant99@gmail.com>
2026-08-01 00:15:52 +0530

    updated the route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -7,7 +7,7 @@ import Contact from "../pages/Contact";
 
 function AppRoutes() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/ReactMindproveSite">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
